eea739b90de5ceffe6cbebd7bf37afee796fe955
imoempire <mrimoempire@gmail.com>
2022-05-28 17:13:39 +0000

    add components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import Home from './Pages/Home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/signup" element={<SignUp/>} />
+    <Route path="/signin" element={<SignIn/>} />
+    <Route path="/" element={<Home/>} />
+  </Routes>
+  </BrowserRouter>
 }
-
-export default App;
